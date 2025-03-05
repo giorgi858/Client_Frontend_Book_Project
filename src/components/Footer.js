65e392ed3ecfe4019f621f791961be3f65c4c63b
giorgi <giorgimtsituri5@gmail.com>
2025-03-05 19:29:08 +0400

    add some babel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,6 @@ const Footer = () => {
   return (
     <footer>
       <h3>Georgian Articles &copy;</h3>
-      {/* <Link  to='https://www.facebook.com/'className='fa-brands fa-facebook'></Link>
-      <Link to='https://www.youtube.com/'className='fa-brands fa-instagram'></Link>
-      <Link to='https://www.youtube.com/'className='fa-brands fa-google-plus'></Link>
-      <Link to='https://www.youtube.com/'className='fa-brands fa-youtube'></Link> */}
     </footer>
   )
 }
